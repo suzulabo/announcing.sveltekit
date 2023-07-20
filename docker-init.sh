0fa2cb747e2b7bb180836a9683e8201a17a373f6
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2023-07-20 07:50:42 +0000

    fix Prettier errors (pnpm in Docker)

--- docker-init.sh
+++ docker-init.sh
@@ -2,7 +2,19 @@
 
 set -ex
 
-mkdir -p ./node_modules/.pnpm-store
-pnpm config set store-dir ./node_modules/.pnpm-store
+
+DIRS=".pnpm-store node_modules packages/viewer/node_modules"
+
+mkdir -p /announcing/data/.pnpm-store
+ln -sf /announcing/data/.pnpm-store .
+mkdir -p /announcing/data/node_modules
+ln -sf /announcing/data/node_modules .
+mkdir -p /announcing/data/packages/viewer/node_modules
+ln -sf /announcing/data/packages/viewer/node_modules packages/viewer
+
+rsync -a /pnpm-store-cache/ .pnpm-store/
 
 pnpm install --frozen-lockfile
+
+pnpm store prune
+rsync -a .pnpm-store/ /pnpm-store-cache/
